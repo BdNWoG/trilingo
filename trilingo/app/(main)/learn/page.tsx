c11c4fcd7b7e4a71bc35360448ba5dfa8980fcbf
BdNWoG <billy.gao@hotmail.com>
2024-11-30 12:19:47 -0800

    save

--- trilingo/app/(main)/learn/page.tsx
+++ trilingo/app/(main)/learn/page.tsx
@@ -1,7 +1,17 @@
+import { FeedWrapper } from "@/components/feed-wrapper";
+import { StickyWrapper } from "@/components/sticky-wrapper";
+import { Header } from "./header";
+import { UserProgress } from "@/components/user-progress";
+
 const LearnPage = () => {
     return (
-        <div>
-            Learn Page
+        <div className="flex flex-row-reverse gap-[48px] px-6">
+            <StickyWrapper>
+                <UserProgress/>
+            </StickyWrapper>
+            <FeedWrapper>
+                <Header title="Spanish"/>
+            </FeedWrapper>
         </div>
     );
 }
--- trilingo/components/mobile-sidebar.tsx
+++ trilingo/components/mobile-sidebar.tsx
@@ -1,6 +1,6 @@
 import { Menu } from "lucide-react";
 import { Sidebar } from "./sidebar";
-import { Sheet, SheetContent, SheetTrigger } from "./ui/sheet";
+import { Sheet, SheetContent, SheetTitle, SheetTrigger } from "./ui/sheet";
 
 export const MobileSidebar = () => {
     return (
@@ -8,7 +8,8 @@ export const MobileSidebar = () => {
             <SheetTrigger>
                 <Menu className="text-white"/>
             </SheetTrigger>
-            <SheetContent>
+            <SheetTitle></SheetTitle>
+            <SheetContent className="p-0 z-[100]" side="left">
                 <Sidebar/>
             </SheetContent>
         </Sheet>
