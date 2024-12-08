dfb238134776e455ba03ef3126b25a4b0c27326b
BdNWoG <billy.gao@hotmail.com>
2024-12-07 21:49:14 -0800

    save

--- trilingo/app/(main)/courses/page.tsx
+++ trilingo/app/(main)/courses/page.tsx
@@ -1,15 +1,18 @@
-import { getCousrses } from "@/db/queries";
+import { getCousrses, getUserProgress } from "@/db/queries";
 import { List } from "./list";
 
 const CoursesPage = async () => {
-    const courses = await getCousrses();
+    const coursesData = getCousrses();
+    const userProgressData = getUserProgress();
+
+    const [courses, userProgress] = await Promise.all([coursesData, userProgressData]);
 
     return (
         <div className="h-full max-w-[912px] px-3 mx-auto">
             <h1 className="text-2xl font-bold text-neutral-700">
                 Language Courses
             </h1>
-            <List courses={courses} activeCourseId={1}/>
+            <List courses={courses} activeCourseId={userProgress?.activeCourseId}/>
         </div>
     );
 }
--- trilingo/db/queries.ts
+++ trilingo/db/queries.ts
@@ -1,8 +1,28 @@
 import { cache } from "react"
 import db from "./drizzle";
+import { auth } from "@clerk/nextjs/server";
+import { userProgress } from "./schema";
+import { eq } from "drizzle-orm";
+
+export const getUserProgress = cache(async() => {
+    const { userId } = await auth();
+
+    if (!userId) {
+        return null;
+    }
+
+    const data = await db.query.userProgress.findFirst({
+        where: eq(userProgress.userId, userId),
+        with: {
+            activeCourse: true,
+        }
+    });
+
+    return data
+})
 
 export const getCousrses = cache(async () => {
     const data = await db.query.courses.findMany();
-    
+
     return data
 });
