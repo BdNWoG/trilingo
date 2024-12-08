22c3de7c6d624a1004e05468e0165f1a3e1f5dde
BdNWoG <billy.gao@hotmail.com>
2024-12-07 20:30:53 -0800

    save

--- trilingo/app/(main)/courses/page.tsx
+++ trilingo/app/(main)/courses/page.tsx
@@ -1,7 +1,15 @@
-const CoursesPage = () => {
+import { getCousrses } from "@/db/queries";
+import { List } from "./list";
+
+const CoursesPage = async () => {
+    const courses = await getCousrses();
+
     return (
-        <div>
-            Courses
+        <div className="h-full max-w-[912px] px-3 mx-auto">
+            <h1 className="text-2xl font-bold text-neutral-700">
+                Language Courses
+            </h1>
+            <List courses={courses} activeCourseId={1}/>
         </div>
     );
 }
