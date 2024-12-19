79436703541f8ea0c2651d0c38ad8dd7982a71f5
BdNWoG <billy.gao@hotmail.com>
2024-12-18 19:09:15 -0800

    save

--- trilingo/scripts/seed.ts
+++ trilingo/scripts/seed.ts
@@ -42,6 +42,62 @@ const main = async () => {
             },
         ]);
 
+        await db.insert(schema.units).values([
+            {
+                id: 1,
+                courseId: 1,
+                title: "Unit 1",
+                description: "Learn the basics of Spanish",
+                order: 1,
+            }
+        ]);
+
+        await db.insert(schema.lessons).values([
+            {
+                id: 1,
+                unitId: 1,
+                title: "Lesson 1",
+                order: 1,
+            },
+        ]);
+
+        await db.insert(schema.challenges).values([
+            {
+                id: 1,
+                lessonId: 1,
+                type: "SELECT",
+                question: "What is the Spanish word for 'the man'?",
+                order: 1,
+            },
+        ]);
+
+        await db.insert(schema.challengeOptions).values([
+            {
+                id: 1,
+                challengeId: 1,
+                imageSrc: "/man.svg",
+                text: "el hombre",
+                correct: true,
+                audioSrc: "/es_man.mp3",
+            },
+            {
+                id: 2,
+                challengeId: 1,
+                imageSrc: "/woman.svg",
+                text: "la mujer",
+                correct: false,
+                audioSrc: "/es_woman.mp3",
+            },
+            {
+                id: 3,
+                challengeId: 1,
+                imageSrc: "/robot.svg",
+                text: "el robot",
+                correct: false,
+                audioSrc: "/es_robot.mp3",
+            },
+        ]);
+
         console.log("Seeded database");
     } catch (error) {
         console.error(error);
