463c7ccda11779cbc30f4a637b124bf47893b6a2
BdNWoG <billy.gao@hotmail.com>
2024-12-26 21:24:07 -0800

    save

---
+++ trilingo/app/lesson/quiz.tsx
@@ -0,0 +1,29 @@
+"use client";
+
+import { challenges, challengeOptions } from "@/db/schema";
+import { useState } from "react";
+import { Header } from "./header";
+
+type Props = {
+    initialPercentage: number;
+    initialHearts: number;
+    initialLessonId: number;
+    initialLessonChallenge: (typeof challenges.$inferSelect & {
+        completed: boolean;
+        challengeOptions: typeof challengeOptions.$inferSelect[];
+    })[];
+    userSubscription: any;
+}
+
+export const Quiz = ( {
+    initialPercentage, initialHearts, initialLessonId, initialLessonChallenge, userSubscription,
+}: Props) => {
+    const [hearts, setHearts] = useState(initialHearts);
+    const [percentage, setPercentage] = useState(initialPercentage);
+
+    return (
+        <>
+            <Header hearts={hearts} percentage={percentage} hasActiveSubscription={!!userSubscription?.isActive}/>
+        </>
+    );
+}
